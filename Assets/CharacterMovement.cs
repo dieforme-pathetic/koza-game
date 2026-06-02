4914f758d1752621985c51750fabb48c09cb755d
troublessohard <clickonrabbit@gmail.com>
2026-06-03 03:02:22 +0500

    маша 2 уровень

--- Assets/CharacterMovement.cs
+++ Assets/CharacterMovement.cs
@@ -341,14 +341,20 @@ public class CharacterMovement : MonoBehaviour
     private void UpdateAnimations()
     {
         if (animator == null) return;
-        
+    
         float currentSpeed = movement.magnitude;
         animator.SetFloat("speed", currentSpeed);
-        
+    
+        if (sr == null) sr = GetComponent<SpriteRenderer>();
+    
         if (movement.x > 0.01f)
-            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        {
+            sr.flipX = false;
+        }
         else if (movement.x < -0.01f)
-            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        {
+            sr.flipX = true;
+        }
     }
     
     private void OnDrawGizmosSelected()
